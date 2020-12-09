56bad49f6e1f8383212b01abe5f8e8ad9c5da650
monicavalverdecalvo <monica22valverde@gmail.com>
2020-12-09 18:11:17 +0100

    CAMARA

--- Vaqlverde/Assets/SCRIPTS/Camara.cs
+++ Vaqlverde/Assets/SCRIPTS/Camara.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Camara : MonoBehaviour
+
 {
+
+    [SerializeField] Transform Sphere;
+    //estas dos variables son para el suavizado de la camara
+    [SerializeField] float smoothVelocity = 0.3F;
+    [SerializeField] Vector3 camaraVelocity = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,11 @@ public class Camara : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        //Con este código, conseguimos que siga al objeto pero con suavidad
+        //La velocidad de suavizado, cuanto menor sea más brusco será el movimiento
+        Vector3 targetPosition = new Vector3(transform.position.x, Sphere.position.y, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref camaraVelocity, smoothVelocity);
+
     }
 }
